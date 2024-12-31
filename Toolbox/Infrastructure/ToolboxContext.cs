da25f3fae83691741d873e429de363ebc6297b47
itheta11 <55739046+itheta11@users.noreply.github.com>
2025-01-01 00:18:47 +0530

    Update ToolboxContext.cs
    Removed connection string

--- Toolbox/Infrastructure/ToolboxContext.cs
+++ Toolbox/Infrastructure/ToolboxContext.cs
@@ -22,11 +22,7 @@ public partial class ToolboxContext : DbContext
     public virtual DbSet<JsonItem> JsonItems { get; set; }
 
     public virtual DbSet<Jsonschema> Jsonschemas { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Server=toolbox.cf42c2woygkb.us-east-1.rds.amazonaws.com,5432;Database=Toolbox;User Id=itheta11;Password=ANUPsweet123;TrustServerCertificate=False;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cheatsheet>(entity =>
